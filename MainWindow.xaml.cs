24dae1575468e26aea7cbc68ed4f6a9e73d8f43f
Кирилл Тепляков <kirillteplycov888@gmail.com>
2024-05-29 17:29:53 +0500

    Программирование окна MainWindow. Тепляков

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,12 @@ namespace ReportGeneration_Тепляков
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            frame.Navigate(new Pages.Main());
         }
     }
 }
