afe9a5c485cb962f69fd732b581cc911cda831c8
KirillColeBou <kirillteplycov888@gmail.com>
2024-05-29 12:21:37 +0500

    Разработанный класс подключения к БД. Тепляков

---
+++ Classes/Common/Connection.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGeneration_Тепляков.Classes.Common
+{
+    public class Connection
+    {
+        public static string config = "server=localhost;uid=root;database=journal;";
+
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+
+        public static MySqlDataReader Query(string SQL, MySqlConnection connection) => new MySqlCommand(SQL, connection).ExecuteReader();
+
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
